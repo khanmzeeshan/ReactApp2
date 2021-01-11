3df376d6e4e159c432df1d672c5e233ad4f80db7
Zeeshan Khan <khanmzeeshan@gmail.com>
2021-01-11 10:51:26 -0800

    Intro Form class to input data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React, { Component } from 'react'
-import Table from './Table'
+import Table from './Table';
+import Form from './Form';
+
 class App extends Component {
-  state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
-  }
-  removeCharacter = index => {
-    const { characters } = this.state
-  
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
-  render() {
-    const { characters } = this.state;
-    
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-      </div>
-    );
-  }
+	state = {
+		characters: []
+  };
+render() {
+return (
+	<div className="container">
+		<Table characterData={characters} removeCharacter={this.removeCharacter} />
+		<Form />
+	</div>
+)
 }
+};
 
-
-export default App
+export default App;
