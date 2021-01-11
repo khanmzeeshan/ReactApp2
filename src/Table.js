6a3003ff66177587df5de95ffe41a34bf4b2a019
Zeeshan Khan <khanmzeeshan@gmail.com>
2021-01-11 10:35:47 -0800

    Completed delete functionality

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
+const Table = props => {
+  const { characterData, removeCharacter } = props
 
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
+}
 const TableHeader = () => {
     return (
       <thead>
@@ -13,27 +22,19 @@ const TableHeader = () => {
   const TableBody = props => {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
       )
     })
   
     return <tbody>{rows}</tbody>
   }
 
-  class Table extends Component {
-    render() {
-      const { characterData } = this.props
-  
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
-  }
+
 
 export default Table
